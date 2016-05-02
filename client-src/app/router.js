ab236e877487963f541c610e696fc87993846a3d
Jeppe Terndrup <jeppe.terndrup@gmail.com>
2016-05-02 14:34:16 +0200

    Base character route

--- client-src/app/router.js
+++ client-src/app/router.js
@@ -1,9 +1,11 @@
-(function (window, $) {
+(function(window, $) {
     window.FC.prototype.router = Backbone.Router.extend({
         routes: {
-            "new": "render_new"
+            "new": "render_new",
+            "app/:region/:server/:charName": "render_char",
+            "app/:region/:server/:charName/:page": "render_char_page"
         },
-        render_new: function () {
+        render_new: function() {
             var newChar = {
                 server: '',
                 region: '',
@@ -11,7 +13,18 @@
                 isFound: false
             }
 
-            new FC.NewCharSplashView(newChar);
+            this.view = new FC.NewCharSplashView(newChar);
+        },
+        /*
+         Renders the base application for the given character combination.
+        */
+        render_char: function(region, server, charName) {
+            // @TODO query backend for data and verification (to verify that the char exists)
+            /* DEBUG */
+            console.log("Hit render_char route with: ", "region: ",region, "server: ", server, "char: ", charName);
+            var newChar = new FC.Character();
+            newChar.set({'region':region, 'server':server, 'name': charName});
+            this.view = new FC.CharacterView(newChar);
         }
     });
-}(window,jQuery));
+}(window, jQuery));
--- client-src/app/views/view.character.js
+++ client-src/app/views/view.character.js
@@ -1,10 +1,12 @@
 (function(window, $, _) {
 
     FC.CharacterView = Backbone.View.extend({
-        tagName: 'div',
-        className: 'charList',
+        el: '#content',
         template: _.template($('#charTemplate').html()),
-
+        initialize: function(newChar) {
+            this.model = newChar;
+            this.render();
+        },
         render: function() {
             this.$el.html(this.template(this.model.attributes));
 
