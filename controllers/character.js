a430d95c7d0595e6f8fe146fe73cf04b081f7c41
Joakim Lindell <joakim@lindell.io>
2016-05-04 16:24:28 +0200

    Normalization code for bnet api talent string

--- controllers/character.js
+++ controllers/character.js
@@ -31,10 +31,13 @@ exports.verifyExists = function(req, res) {
         name: name,
         fields: ['profile', 'items', 'talents', 'stats']
     }, function(err, body, response) {
+        
+        // TODO: Replace Blood with Havoc.
+        body.talents = normaliseCalcTalent(findCalcTalentByClassSpec("Blood", body.talents));
 
         if(body.detail === "Account Inactive" && body.type === "Forbidden") {
-          console.log("API key does not seem to be configured. Please setup config/local.js");
-          res.status(403).send({ "type": body.type, "detail": body.detail });
+            console.log("API key does not seem to be configured. Please setup config/local.js");
+            res.status(403).send({ "type": body.type, "detail": body.detail });
         } else if (!body.status && !body.reason) {
             res.status(200).send(body);
         } else if (body.reason === "Character not found.") {
@@ -43,6 +46,37 @@ exports.verifyExists = function(req, res) {
             res.status(500).send({ "status": body.status, "reason": body.reason });
         }
     });
+    
+    /* Normalise talent data from Battle.net API for Felcraft
+     * Example:
+     * string calcTalent = "1000002" 
+     * converts into:
+     * Array  arrTalents = [[0,1,0],[1,0,0]...[0,0,1]]  */
+    function normaliseCalcTalent(calcTalent) {
+        var arrTalents = new Array(); 
+        for (x=0; x<calcTalent.length; x++) {
+            var tempArr = [0,0,0];
+	    tempArr[calcTalent[x]]=1;
+            arrTalents.push(tempArr);
+        }                  
+    	return arrTalents;
+    }
+    
+    /* Finds the calcTalent string from the first spec matching given name 
+     * Example:
+     * string spec = "Brewmaster" 
+     * dataTalentArr[] expected to contain talents array from
+     * the Battle.net API. 
+     * */
+    function findCalcTalentByClassSpec(spec, dataTalentArr) {
+        for(x=0; x<dataTalentArr.length; x++) {
+            if(dataTalentArr[x].spec.name === spec) {
+              return dataTalentArr[x].calcTalent;
+            } 
+        }
+        console.log("Failed to find talent distribution.");
+        return "0000000"; //failback
+    }
 
     // temporary always OK response
     //res.status(200).send({ 'message': 'OK' });
